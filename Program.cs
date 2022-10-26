f207af8ca5d1608a814f5354075cbe284e582c20
Lev <Dmslmrk@icloud.com>
2022-10-26 13:36:33 +0300

    добавлен код

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Write("Введите размер массива:  ");
+int Size = Convert.ToInt32(Console.ReadLine());
+string [] Array = new string [Size];
+Console.WriteLine("Вводите значения элементов:   ");
+for(int i = 0; i < Size; i++)
+{
+   Array[i] = Console.ReadLine();
+}
+Console.Write("Полученный массив:   ");
+Console.WriteLine(string.Join(", ", Array));
+
+string word=Array[0];
+int Count=0;
+
+for (int i = 0; i < Size; i++)
+{
+   word=Array[i];
+   if(word.Length<=3)
+   {
+      Count++;
+   }
+}
+int j=0;
+string [] Less3Array = new string [Count];
+
+
+for (int i = 0; i < Size; i++)
+{
+   word=Array[i];
+   if( word.Length <=3)
+   {
+      Less3Array[j]=Array[i];
+      j++;
+   }
+}
+
+Console.Write("Отсортированный массив элементов:   ");
+Console.WriteLine(string.Join(", ", Less3Array));
